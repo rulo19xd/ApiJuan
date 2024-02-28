02577801be7afb5540f672750c6b4202c881b488
Juan Pablo <juampicortez19xd@gmail.com>
2024-02-28 15:06:57 -0300

    Proyecto Api

---
+++ ApiJuan/ApiJuan/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ApiJuan.Models;
+
+namespace ApiJuan.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
